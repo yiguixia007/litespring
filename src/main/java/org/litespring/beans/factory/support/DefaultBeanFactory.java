c1b25fcd041a4193f5a898e5772616dfee0f6464
yiguixia007 <157026488@qq.com>
2020-02-12 15:46:49 +0800

    实现DefaultBeanFactory

--- src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -3,15 +3,93 @@ package org.litespring.beans.factory.support;
 import org.litespring.beans.factory.BeanDefinition;
 import org.litespring.beans.factory.BeanFactory;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.litespring.beans.factory.BeanFactory;
+import org.litespring.util.ClassUtils;
+
 public class DefaultBeanFactory implements BeanFactory {
+    private static final String ID_ATTRIBUTE = "id";
+    private static final String CLASS_ATTRIBUTE = "class";
+    private final Map<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<String, BeanDefinition>(64);
+
     public DefaultBeanFactory(String configFile) {
+        loadBeanDefinition(configFile);
+    }
+
+    private void loadBeanDefinition(String configFile) {
+        InputStream is = null;
+        try {
+            // 1、将xml文件转换成xml文件
+            ClassLoader cl = ClassUtils.getDefaultClassLoader();
+            is = cl.getResourceAsStream(configFile);
+
+            // 2、将xml转换成Document
+            SAXReader reader = new SAXReader();
+            Document doc = reader.read(is);
+
+            // 3、解析出bean的定义并存储到容器中
+            Element root = doc.getRootElement(); //<beans>
+            Iterator<Element> iter = root.elementIterator();
+            while (iter.hasNext()) {
+                Element ele = iter.next();
+                String id = ele.attributeValue(ID_ATTRIBUTE);
+                String beanClassName = ele.attributeValue(CLASS_ATTRIBUTE);
+                BeanDefinition bd = new GenericBeanDefinition(id, beanClassName);
+                this.beanDefinitionMap.put(id, bd);
+            }
+        } catch (DocumentException e) {
+            //TODO 抛出异常
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public BeanDefinition getBeanDefinition(String beanID) {
-        return null;
+        return this.beanDefinitionMap.get(beanID);
     }
 
+    /**
+     * 获取bean的实例
+     * @param beanID beanID
+     * @return bean的实例
+     */
     public Object getBean(String beanID) {
+        BeanDefinition bd = this.getBeanDefinition(beanID);
+        if (bd == null) {
+            return null;
+        }
+        ClassLoader cl = ClassUtils.getDefaultClassLoader();
+        String beanClassName = bd.getBeanClassName();
+        try {
+            Class<?> clz = cl.loadClass(beanClassName);
+            return clz.newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+
+            e.printStackTrace();
+        }
         return null;
     }
 }
---
+++ src/main/java/org/litespring/beans/factory/support/GenericBeanDefinition.java
@@ -0,0 +1,20 @@
+package org.litespring.beans.factory.support;
+
+import org.litespring.beans.factory.BeanDefinition;
+
+public class GenericBeanDefinition implements BeanDefinition {
+    private String id;
+    private String beanClassName;
+
+    public GenericBeanDefinition(String id, String beanClassName) {
+
+        this.id = id;
+        this.beanClassName = beanClassName;
+    }
+
+    public String getBeanClassName() {
+
+        return this.beanClassName;
+    }
+
+}
